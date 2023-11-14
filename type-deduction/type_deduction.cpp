937219031d4fdac70e552530cd193afebcc7dc16
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-14 08:58:08 +0100

    Update code

--- type-deduction/type_deduction.cpp
+++ type-deduction/type_deduction.cpp
@@ -45,52 +45,53 @@ TEST_CASE("type deduction rules")
     SECTION("Case 1")
     {
         deduce1(x);
-        deduce1(ref_x);  // ref is removed
-        deduce1(cref_x); // ref & const are removed
-        deduce1(tab);    // int* - array decays to pointer
-        deduce1(foo);    // void (*)(int)
-        deduce1(cx);     // const is removed
+        deduce1(cx);     
+        deduce1(ref_x);  
+        deduce1(cref_x); 
+        deduce1(tab);    
+        deduce1(foo);    
 
         auto a1 = x;
-        auto a2 = ref_x;
-        auto a3 = cref_x;
-        auto a4 = tab;
-        auto a5 = foo;
-        auto a6 = cx; // int
+        auto a2 = cx; 
+        auto a3 = ref_x;
+        auto a4 = cref_x;
+        auto a5 = tab;
+        auto a6 = foo;
     }
 
     SECTION("Case 2")
     {
-        deduce2(x);      // ref is added
-        deduce2(ref_x);  // ref is preserved
-        deduce2(cref_x); // ref & const are preserved
-        deduce2(tab);    // tab size is preserved
-        deduce2(foo);    // void (&)(int)
-        deduce2(cx);     // const int&
-
-        auto& a1 = x;      // int&
-        auto& a2 = ref_x;  // int&
-        auto& a3 = cref_x; // const int&
-        auto& a4 = tab;    // int(&a4)[10]
-        auto& a5 = foo;    // void (&)(int)
+        deduce2(x);      
+        deduce2(cx);     
+        deduce2(ref_x);  
+        deduce2(cref_x); 
+        deduce2(tab);    
+        deduce2(foo);    
+        
+        auto& a1 = x;   
+        auto& a2 = cx;   
+        auto& a3 = ref_x;  
+        auto& a4 = cref_x; 
+        auto& a5 = tab;    
+        auto& a6 = foo;    
     }
 
     SECTION("Case 3")
     {
-        deduce3(x);              // deduce3<int&>(int&)
-        deduce3(ref_x);          // deduce3<int&>(int&)
-        deduce3(cref_x);         // deduce<const int&>(const int&)
-        deduce3(tab);            // deduce3<int(&)[10]>(int(&)[10])
-        deduce3(foo);            // deduce3<void (&)(int)>(void (&)(int))
-        deduce3(cx);             // deduce3<const int&>(const int&)
-        deduce3(string("text")); // deduce4<string>(string&&)
+        deduce3(x);              
+        deduce3(ref_x);          
+        deduce3(cref_x);         
+        deduce3(tab);            
+        deduce3(foo);            
+        deduce3(cx);             
+        deduce3(string("text")); 
 
-        auto&& a1 = x;      // int&
-        auto&& a2 = ref_x;  // int&
-        auto&& a3 = cref_x; // const int&
-        auto&& a4 = tab;    // int(&a4)[10]
-        auto&& a5 = foo;    // void (&)(int)
-        auto&& a6 = cx;
+        auto&& a1 = x;      
+        auto&& a2 = cx;
+        auto&& a3 = ref_x;  
+        auto&& a4 = cref_x; 
+        auto&& a5 = tab;    
+        auto&& a6 = foo;    
         auto&& a7 = string("text");
     }
 }
