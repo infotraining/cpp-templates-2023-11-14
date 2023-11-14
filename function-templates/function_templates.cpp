a9b3789052cd7d9be1d0cb29a02990679c484e82
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-11-14 12:12:30 +0100

    auto type deduction

--- function-templates/function_templates.cpp
+++ function-templates/function_templates.cpp
@@ -5,7 +5,141 @@
 
 using namespace std::literals;
 
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#endif
+
+struct Id
+{
+    int value;
+    std::string name;
+
+    // Id(int v, const std::string& n) : value(v), name(n)
+    // {}
+
+    // bool operator==(const Id& other) const = default; // implicitly declared
+    auto operator<=>(const Id& other) const = default;
+};
+
+template <typename T>
+T max_value(T a, T b)
+{
+    return a < b ? b : a;
+}
+
+template <> // full specialization of template function
+const char* max_value(const char* a, const char* b)
+{
+    std::cout << __PRETTY_FUNCTION__ << " - template full specialization!\n";
+    return std::strcmp(a, b) < 0 ? b : a;
+}
+
+const char* max_value(const char* a, const char* b) // overloading
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+    return std::strcmp(a, b) < 0 ? b : a;
+}
+
+namespace Cpp20
+{
+    auto max_value(auto a, auto b)
+    {
+        return a < b ? b : a;
+    }
+
+    namespace IsInterpretedAs
+    {
+        template <typename T1, typename T2>
+        auto max_value(T1 a, T2 b)
+        {
+            return a < b ? b : a;
+        }
+    } // namespace IsInterpretedAs
+} // namespace Cpp20
+
 TEST_CASE("function templates")
 {
-    CHECK(true);
+    CHECK(max_value(42, 665) == 665);
+
+    CHECK(max_value(43.0, 66.5) == 66.5);
+
+    std::string s1 = "text";
+    std::string s2 = "test";
+    CHECK(max_value(s1, s2) == "text"s);
+
+    CHECK(max_value(static_cast<double>(42), 66.5) == 66.5);
+    CHECK(max_value<double>(42, 66.5) == 66.5);
+    // CHECK(Cpp20::max_value(67, 66.5) == 67.0);
+
+    CHECK(max_value(Id(42, "forty-two"), Id{665}) == Id{665});
+
+    CHECK(max_value<const char*>("ala", "ola") == "ola"s);
+    CHECK(max_value("ala", "ola") == "ola"s);
+
+    const char* (*ptr_fun)(const char*, const char*) = max_value;
+    CHECK(ptr_fun("ala", "ola") == "ola"s);
+
+    ptr_fun = max_value<const char*>;
+    CHECK(ptr_fun("ala", "ola") == "ola"s);
+
+    auto ptr_fun_other = max_value<double>;
+}
+
+TEST_CASE("uniform syntax")
+{
+    auto ptr = std::make_shared<Id>(42, "forty-two"); // C++11 - C++17 - compiler error; C++20 - OK
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+namespace ReturnType
+{
+    namespace ExplicitParam
+    {
+        template <typename TResult, typename T1, typename T2>
+        TResult max_value(T1 a, T2 b)
+        {
+            return a < b ? b : a;
+        }
+    } // namespace ExplicitParam
+
+    namespace Auto
+    {
+        template <typename T1, typename T2>
+        auto max_value(T1 a, T2 b)
+        {
+            return a < b ? b : a;
+        }
+    }
+
+    namespace TypeTraits
+    {
+        template <typename T1, typename T2>
+        std::common_type_t<T1, T2> max_value(T1 a, T2 b)
+        {
+            return a < b ? b : a;
+        }
+    }
+
+    namespace DefaultParam
+    {
+        template <typename T1, typename T2, typename TResult = std::common_type_t<T1, T2>>
+        TResult max_value(T1 a, T2 b)
+        {
+            return a < b ? b : a;
+        }
+    }
+} // namespace ReturnType
+
+TEST_CASE("return type")
+{ 
+    CHECK(ReturnType::ExplicitParam::max_value<double>(42, 66.5) == 66.5);
+    
+    CHECK(ReturnType::Auto::max_value(42, 66.5) == 66.5);
+
+    CHECK(ReturnType::TypeTraits::max_value(42, 66.5) == 66.5);
+    CHECK(ReturnType::TypeTraits::max_value("text"s, "test") == "text"s);
+    
+    CHECK(ReturnType::DefaultParam::max_value(42, 66.5f) == 66.5f);
+    CHECK(ReturnType::DefaultParam::max_value<int, float, double>(42, 66.5f) == 66.5f);
 }
--- type-deduction/type_deduction.cpp
+++ type-deduction/type_deduction.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #endif
 
 void foo(int)
-{ }
+{}
 
 template <typename T>
 void deduce1(T arg)
@@ -37,62 +37,68 @@ void deduce3(T&& arg)
 TEST_CASE("type deduction rules")
 {
     int x = 10;
+    const int cx = 10;
     int& ref_x = x;
     const int& cref_x = x;
     int tab[10];
-    const int cx = 10;
 
     SECTION("Case 1")
     {
-        deduce1(x);
-        deduce1(cx);     
-        deduce1(ref_x);  
-        deduce1(cref_x); 
-        deduce1(tab);    
-        deduce1(foo);    
+        deduce1(x);       // deduce1<int>(int)
+        deduce1(cx);      // deduce1<int>(int)
+        deduce1(ref_x);   // deduce1<int>(int)
+        deduce1(cref_x);  // deduce1<int>(int)
+        deduce1(tab);     // deduce1<int*>(int*)
+        deduce1(foo);     // deduce1<void(*)(int)>(void(*)(int))
 
-        auto a1 = x;
-        auto a2 = cx; 
-        auto a3 = ref_x;
-        auto a4 = cref_x;
-        auto a5 = tab;
-        auto a6 = foo;
+        auto a1 = x;      // int
+        auto a2 = cx;     // int
+        auto a3 = ref_x;  // int
+        auto a4 = cref_x; // int
+        auto a5 = tab;    // int*
+        auto a6 = foo;    // void(*a6)(int)
+
+        auto a7 = {1, 2, 3};   // std::initializer_list<int>
+        // deduce1({1, 2, 3}); // ERROR
     }
 
     SECTION("Case 2")
     {
-        deduce2(x);      
-        deduce2(cx);     
-        deduce2(ref_x);  
-        deduce2(cref_x); 
-        deduce2(tab);    
-        deduce2(foo);    
+        deduce2(x);        // deduce2<int>(int&)
+        deduce2(cx);       // deduce2<const int>(const int&)
+        deduce2(ref_x);    // deduce2<int>(int&)
+        deduce2(cref_x);   // deduce2<const int>(const int&)
+        deduce2(tab);      // deduce2<int[10]>(int(&arg)[10])
+        deduce2(foo);      // deduce2<void(int)>(void(&arg)(int))
         
-        auto& a1 = x;   
-        auto& a2 = cx;   
-        auto& a3 = ref_x;  
-        auto& a4 = cref_x; 
-        auto& a5 = tab;    
-        auto& a6 = foo;    
+        auto& a1 = x;      // int&
+        auto& a2 = cx;     // const int&
+        auto& a3 = ref_x;  // int&
+        auto& a4 = cref_x; // const int&
+        auto& a5 = tab;    // int(&a5)[10]
+        auto& a6 = foo;    // void(&a6)(int)
     }
 
     SECTION("Case 3")
     {
-        deduce3(x);              
-        deduce3(ref_x);          
-        deduce3(cref_x);         
-        deduce3(tab);            
-        deduce3(foo);            
-        deduce3(cx);             
-        deduce3(string("text")); 
+        // l-value
+        deduce3(x);               // deduce3<int&>(int& &&) -> deduce3<int&>(int&)
+        deduce3(cx);              // deduce3<const int&>(const int& &&) -> deduce3<const int&>(const int&)
+        deduce3(ref_x);           // deduce3<int&>(int& &&) -> deduce3<int&>(int&)
+        deduce3(cref_x);          // deduce3<const int&>(const int& &&) -> deduce3<const int&>(const int&)
+        deduce3(tab);             // deduce3<int(&)[10]>(int(&)[10])
+        deduce3(foo);             // deduce3<void(&)(int)>(void(&)(int))
 
-        auto&& a1 = x;      
-        auto&& a2 = cx;
-        auto&& a3 = ref_x;  
-        auto&& a4 = cref_x; 
-        auto&& a5 = tab;    
-        auto&& a6 = foo;    
-        auto&& a7 = string("text");
+        // r-value
+        deduce3(string("text"));  // deduce3<std::string>(std::string&&)
+
+        auto&& a1 = x;                   // int&
+        auto&& a2 = cx;                  // const int&
+        auto&& a3 = ref_x;               // int&
+        auto&& a4 = cref_x;              // const int&
+        auto&& a5 = tab;                 // int(&)[10]
+        auto&& a6 = foo;                 // void(&)(int)
+        auto&& a7 = string("text");      // std::string&&
     }
 }
 
